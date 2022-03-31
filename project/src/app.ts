04424f955b19dcde7cde7b9ece80675b7b8d2e01
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-01 00:34:16 +0900

    점진적인 타입 적용중...

--- project/src/app.ts
+++ project/src/app.ts
@@ -1,33 +1,34 @@
 // utils
-function $(selector) {
+function $(selector: string) {
   return document.querySelector(selector);
 }
-function getUnixTimestamp(date) {
+function getUnixTimestamp(date: Date) {
   return new Date(date).getTime();
 }
 
 // DOM
-const confirmedTotal = $('.confirmed-total');
-const deathsTotal = $('.deaths');
-const recoveredTotal = $('.recovered');
-const lastUpdatedTime = $('.last-updated-time');
-const rankList = $('.rank-list');
-const deathsList = $('.deaths-list');
-const recoveredList = $('.recovered-list');
-const deathSpinner = createSpinnerElement('deaths-spinner');
-const recoveredSpinner = createSpinnerElement('recovered-spinner');
+var a: Element | HTMLElement | HTMLParagraphElement;
+const confirmedTotal = $(".confirmed-total") as HTMLSpanElement;
+const deathsTotal = $(".deaths") as HTMLParagraphElement;
+const recoveredTotal = $(".recovered") as HTMLParagraphElement;
+const lastUpdatedTime = $(".last-updated-time") as HTMLParagraphElement;
+const rankList = $(".rank-list");
+const deathsList = $(".deaths-list");
+const recoveredList = $(".recovered-list");
+const deathSpinner = createSpinnerElement("deaths-spinner");
+const recoveredSpinner = createSpinnerElement("recovered-spinner");
 
-function createSpinnerElement(id) {
-  const wrapperDiv = document.createElement('div');
-  wrapperDiv.setAttribute('id', id);
+function createSpinnerElement(id: string) {
+  const wrapperDiv = document.createElement("div");
+  wrapperDiv.setAttribute("id", id);
   wrapperDiv.setAttribute(
-    'class',
-    'spinner-wrapper flex justify-center align-center',
+    "class",
+    "spinner-wrapper flex justify-center align-center"
   );
-  const spinnerDiv = document.createElement('div');
-  spinnerDiv.setAttribute('class', 'ripple-spinner');
-  spinnerDiv.appendChild(document.createElement('div'));
-  spinnerDiv.appendChild(document.createElement('div'));
+  const spinnerDiv = document.createElement("div");
+  spinnerDiv.setAttribute("class", "ripple-spinner");
+  spinnerDiv.appendChild(document.createElement("div"));
+  spinnerDiv.appendChild(document.createElement("div"));
   wrapperDiv.appendChild(spinnerDiv);
   return wrapperDiv;
 }
@@ -38,11 +39,17 @@ let isRecoveredLoading = false;
 
 // api
 function fetchCovidSummary() {
-  const url = 'https://api.covid19api.com/summary';
+  const url = "https://api.covid19api.com/summary";
   return axios.get(url);
 }
 
-function fetchCountryInfo(countryCode, status) {
+enum CovidStatus {
+  Confirmed = "confirmed",
+  Recovered = "recovered",
+  Deaths = "deaths",
+}
+
+function fetchCountryInfo(countryCode: string, status: CovidStatus) {
   // params: confirmed, recovered, deaths
   const url = `https://api.covid19api.com/country/${countryCode}/status/${status}`;
   return axios.get(url);
@@ -56,10 +63,10 @@ function startApp() {
 
 // events
 function initEvents() {
-  rankList.addEventListener('click', handleListClick);
+  rankList.addEventListener("click", handleListClick);
 }
 
-async function handleListClick(event) {
+async function handleListClick(event: any) {
   let selectedId;
   if (
     event.target instanceof HTMLParagraphElement ||
@@ -77,14 +84,17 @@ async function handleListClick(event) {
   clearRecoveredList();
   startLoadingAnimation();
   isDeathLoading = true;
-  const { data: deathResponse } = await fetchCountryInfo(selectedId, 'deaths');
+  const { data: deathResponse } = await fetchCountryInfo(
+    selectedId,
+    CovidStatus.Deaths
+  );
   const { data: recoveredResponse } = await fetchCountryInfo(
     selectedId,
-    'recovered',
+    CovidStatus.Recovered
   );
   const { data: confirmedResponse } = await fetchCountryInfo(
     selectedId,
-    'confirmed',
+    CovidStatus.Confirmed
   );
   endLoadingAnimation();
   setDeathsList(deathResponse);
@@ -95,17 +105,17 @@ async function handleListClick(event) {
   isDeathLoading = false;
 }
 
-function setDeathsList(data) {
+function setDeathsList(data: any) {
   const sorted = data.sort(
-    (a, b) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date),
+    (a: any, b: any) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
   );
-  sorted.forEach(value => {
-    const li = document.createElement('li');
-    li.setAttribute('class', 'list-item-b flex align-center');
-    const span = document.createElement('span');
+  sorted.forEach((value: any) => {
+    const li = document.createElement("li");
+    li.setAttribute("class", "list-item-b flex align-center");
+    const span = document.createElement("span");
     span.textContent = value.Cases;
-    span.setAttribute('class', 'deaths');
-    const p = document.createElement('p');
+    span.setAttribute("class", "deaths");
+    const p = document.createElement("p");
     p.textContent = new Date(value.Date).toLocaleDateString().slice(0, -1);
     li.appendChild(span);
     li.appendChild(p);
@@ -117,21 +127,21 @@ function clearDeathList() {
   deathsList.innerHTML = null;
 }
 
-function setTotalDeathsByCountry(data) {
+function setTotalDeathsByCountry(data: any) {
   deathsTotal.innerText = data[0].Cases;
 }
 
 function setRecoveredList(data) {
   const sorted = data.sort(
-    (a, b) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date),
+    (a, b) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
   );
-  sorted.forEach(value => {
-    const li = document.createElement('li');
-    li.setAttribute('class', 'list-item-b flex align-center');
-    const span = document.createElement('span');
+  sorted.forEach((value) => {
+    const li = document.createElement("li");
+    li.setAttribute("class", "list-item-b flex align-center");
+    const span = document.createElement("span");
     span.textContent = value.Cases;
-    span.setAttribute('class', 'recovered');
-    const p = document.createElement('p');
+    span.setAttribute("class", "recovered");
+    const p = document.createElement("p");
     p.textContent = new Date(value.Date).toLocaleDateString().slice(0, -1);
     li.appendChild(span);
     li.appendChild(p);
@@ -167,18 +177,18 @@ async function setupData() {
 }
 
 function renderChart(data, labels) {
-  var ctx = $('#lineChart').getContext('2d');
-  Chart.defaults.color = '#f5eaea';
-  Chart.defaults.font.family = 'Exo 2';
+  var ctx = $("#lineChart").getContext("2d");
+  Chart.defaults.color = "#f5eaea";
+  Chart.defaults.font.family = "Exo 2";
   new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels,
       datasets: [
         {
-          label: 'Confirmed for the last two weeks',
-          backgroundColor: '#feb72b',
-          borderColor: '#feb72b',
+          label: "Confirmed for the last two weeks",
+          backgroundColor: "#feb72b",
+          borderColor: "#feb72b",
           data,
         },
       ],
@@ -188,47 +198,47 @@ function renderChart(data, labels) {
 }
 
 function setChartData(data) {
-  const chartData = data.slice(-14).map(value => value.Cases);
+  const chartData = data.slice(-14).map((value) => value.Cases);
   const chartLabel = data
     .slice(-14)
-    .map(value => new Date(value.Date).toLocaleDateString().slice(5, -1));
+    .map((value) => new Date(value.Date).toLocaleDateString().slice(5, -1));
   renderChart(chartData, chartLabel);
 }
 
 function setTotalConfirmedNumber(data) {
   confirmedTotal.innerText = data.Countries.reduce(
     (total, current) => (total += current.TotalConfirmed),
-    0,
+    0
   );
 }
 
 function setTotalDeathsByWorld(data) {
   deathsTotal.innerText = data.Countries.reduce(
     (total, current) => (total += current.TotalDeaths),
-    0,
+    0
   );
 }
 
 function setTotalRecoveredByWorld(data) {
   recoveredTotal.innerText = data.Countries.reduce(
     (total, current) => (total += current.TotalRecovered),
-    0,
+    0
   );
 }
 
 function setCountryRanksByConfirmedCases(data) {
   const sorted = data.Countries.sort(
-    (a, b) => b.TotalConfirmed - a.TotalConfirmed,
+    (a, b) => b.TotalConfirmed - a.TotalConfirmed
   );
-  sorted.forEach(value => {
-    const li = document.createElement('li');
-    li.setAttribute('class', 'list-item flex align-center');
-    li.setAttribute('id', value.Slug);
-    const span = document.createElement('span');
+  sorted.forEach((value) => {
+    const li = document.createElement("li");
+    li.setAttribute("class", "list-item flex align-center");
+    li.setAttribute("id", value.Slug);
+    const span = document.createElement("span");
     span.textContent = value.TotalConfirmed;
-    span.setAttribute('class', 'cases');
-    const p = document.createElement('p');
-    p.setAttribute('class', 'country');
+    span.setAttribute("class", "cases");
+    const p = document.createElement("p");
+    p.setAttribute("class", "country");
     p.textContent = value.Country;
     li.appendChild(span);
     li.appendChild(p);
